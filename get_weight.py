a8e54296d8b914ee6625954e2da7d4104f440488
seolgi <32099414+seolgi@users.noreply.github.com>
2024-03-29 18:58:15 +0900

    Update get_weight.py

--- get_weight.py
+++ get_weight.py
@@ -7,6 +7,6 @@ if not os.path.isdir(path_weight):
    os.makedirs(path_weight)
    
 
-url = 'https://drive.google.com/file/d/1ZkU5psrtBaNwn7AWZ2XrOdv8P8CammlF/view?usp=share_link'
+url = 'https://drive.google.com/file/d/13U0Dp6tfAGAw0fWQ_zdlWCvi_AdEhosh/view?usp=drive_link'
 output = f'{path_weight}best.pt'
 gdown.download(url, output, quiet=False, fuzzy=True)
